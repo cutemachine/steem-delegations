6e129e7a76af98f3ae2a074c2b976f08dfe2adda
Jo Meenen <jo@meenen.de>
2018-03-04 17:32:21 +0100

    Add accountHistory and delegationHistory

--- src/state/index.js
+++ src/state/index.js
@@ -1,9 +1,12 @@
 export default {
+  error: {
+    message: ''
+  },
   location: location.state,
   isNavbarMenuActive: false,
   username: '',
   delegations: [],
   dynamicGlobalProperties: {},
+  accountHistory: [],
+  delegationHistory: []
 }
-
-

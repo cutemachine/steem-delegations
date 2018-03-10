7caa40c767f731b3457d7cd8e71aed13daeebe35
Jo Meenen <jo@meenen.de>
2018-03-10 14:17:10 +0100

    Add utility function to convert Steem to Vests

--- src/common/utils.js
+++ src/common/utils.js
@@ -1,7 +1,7 @@
 // Strip unit (VESTS, STEEM, SBD) from string and return a Number
 export const unitString2Number = (stringWithUnit) => Number(stringWithUnit.split(' ')[0])
 
-// Convert vests to Steem
+// Convert Vests to Steem
 export const vests2Steem = (vestingShares, dynamicGlobalProperties) => {
   const { total_vesting_fund_steem, total_vesting_shares } = dynamicGlobalProperties
   const totalVestingFundSteemNumber = unitString2Number(total_vesting_fund_steem)
@@ -9,4 +9,14 @@ export const vests2Steem = (vestingShares, dynamicGlobalProperties) => {
   const vestingSharesNumber = unitString2Number(vestingShares)
 
   return (totalVestingFundSteemNumber * (vestingSharesNumber / totalVestingSharesNumber)).toFixed(6)
+}
+
+// Convert Steem to Vests
+export const steem2Vests = (steem, dynamicGlobalProperties) => {
+  const { total_vesting_fund_steem, total_vesting_shares } = dynamicGlobalProperties
+  const totalVestingFundSteemNumber = unitString2Number(total_vesting_fund_steem)
+  const totalVestingSharesNumber = unitString2Number(total_vesting_shares)
+
+  const vests = steem / (totalVestingFundSteemNumber / totalVestingSharesNumber)
+  return vests.toFixed(6)
 }
